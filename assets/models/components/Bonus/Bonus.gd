7161607efd036280c1ccb5710ecd8d718013f8db
Zuck <julienlesne17@gmail.com>
2024-11-23 19:30:30 +0100

    everything is working fine

--- assets/models/components/Bonus/Bonus.gd
+++ assets/models/components/Bonus/Bonus.gd
@@ -1,8 +1,7 @@
 extends Node3D
 
-# Variables de gestion du bonus 
-@export var bonus_type : String # Type de bonus (par exemple, "speed", "shield", "repair")
-var is_collected = false
+# Variables de gestion du bonus
+@export var bonus_type : String = "health"
 
 # Référence au nœud AudioStreamPlayer
 @onready var audio_player = $AudioStreamPlayer
@@ -19,6 +18,15 @@ func _process(delta: float) -> void:
 # Fonction de collecte
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	print("bonus !")
+	# (exemple)
+	if body.is_in_group("car"): # Vérifie si l'objet est un joueur
+		apply_bonus(body) # Appliquer l'effet du bonus au joueur
+		# Appeler une fonction async pour gérer le son et la suppression
+		_play_sound_and_remove()
+	pass # Replace with function body.
+
+# Fonction async qui gère le son et la suppression
+func _play_sound_and_remove():
 	var temp_audio_player = AudioStreamPlayer.new() # Créer un AudioStreamPlayer temporaire
 	temp_audio_player.stream = audio_player.stream # Utiliser le même son
 	get_parent().add_child(temp_audio_player) # Ajouter au parent
@@ -26,21 +34,13 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	queue_free() # Supprimer immédiatement l'objet
 	await temp_audio_player.finished # Attendre le signal 'finished' du temp_audio_player
 	temp_audio_player.queue_free() # Supprimer le player temporaire après la fin du son
-	
-	# (exemple)
-	if body.is_in_group("player"): # Vérifie si l'objet est un joueur
-		if !is_collected:
-			is_collected = true
-			apply_bonus(body) # Appliquer l'effet du bonus au joueur
-			queue_free() # Supprimer le bonus de l'arène après avoir été collecté
-	pass # Replace with function body.
 
 # Appliquer le bonus au joueur (exemple)
 func apply_bonus(player: Node):
 	match bonus_type:
+		"health":
+			player.heal()
 		"speed":
 			player.apply_speed_boost()
 		"shield":
 			player.apply_shield()
-		"repair":
-			player.apply_repair()
--- cars/BaseCar.gd
+++ cars/BaseCar.gd
@@ -102,6 +102,12 @@ func _physics_process(delta):
 		$wheal3.wheel_friction_slip=3
 	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 
+func heal():
+	life += 1;
+	if life > default_life:
+		life = default_life;
+	$SubViewport/HealthBar.value = life
+
 func damage(damage: int):
 	life-=damage
 	$SubViewport/HealthBar.value = life
