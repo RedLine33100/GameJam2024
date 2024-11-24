40bb9e3f354a20e871c416589b01a8437a813059
Zuck <julienlesne17@gmail.com>
2024-11-24 15:04:40 +0100

    fix double pickup bonus

--- assets/models/components/Bonus/Bonus.gd
+++ assets/models/components/Bonus/Bonus.gd
@@ -20,6 +20,11 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	print("bonus !")
 	# (exemple)
 	if body is VehicleBody3D:
+		
+		# Désactiver toutes les interactions
+		self.visible = false
+		$Area3D/CollisionShape3D.queue_free()
+		
 		var car : VehicleBody3D = body
 		car.applyRandomBonus()
 		# Appeler une fonction async pour gérer le son et la suppression
@@ -30,9 +35,12 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 func _play_sound_and_remove():
 	var temp_audio_player = AudioStreamPlayer.new() # Créer un AudioStreamPlayer temporaire
 	temp_audio_player.stream = audio_player.stream # Utiliser le même son
-	get_parent().add_child(temp_audio_player) # Ajouter au parent
+	add_child(temp_audio_player) # Ajouter au parent
 	temp_audio_player.play() # Jouer le son
-	self.visible = false
-	await temp_audio_player.finished # Attendre le signal 'finished' du temp_audio_player
+	
+	# Attendre que le son soit terminé
+	while temp_audio_player.is_playing():
+		await get_tree().process_frame
+	
 	temp_audio_player.queue_free() # Supprimer le player temporaire après la fin du son
 	queue_free() # Supprimer immédiatement l'objet
