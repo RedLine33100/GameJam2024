2399bbc75328b83222edf15aba0bedb6cb54d82d
Nalil10 <lilian.mar@free.fr>
2024-11-24 09:52:59 +0100

    feta : game over

--- scenes/game_over_menu/game_over.gd
+++ scenes/game_over_menu/game_over.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var quit_button = $MarginContainer/VBoxContainer/Quit_Button as Button
-@onready var mainmenu_button = $MarginContainer/VBoxContainer/MainMenu_Button as Button
+@onready var quit_button = $MarginContainer/VBoxContainer2/Quit_Button as Button
+@onready var mainmenu_button = $MarginContainer/VBoxContainer2/MainMenu_Button as Button
 @onready var winner_label = $MarginContainer/VBoxContainer/WinnerLabel as Label
 @onready var mainMenu = preload("res://scenes/main_menu/main_menu.tscn") as PackedScene
 @onready var game_scene = preload("res://scenes/car.tscn") as PackedScene
